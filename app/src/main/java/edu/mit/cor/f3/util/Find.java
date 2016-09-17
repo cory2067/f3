eb68619d84715b5dc7a1a968ebe55060b786260b
Cory Lynch <cjl2625@gmail.com>
2016-09-17 19:42:28 -0400

    fixed the finder

--- app/src/main/java/edu/mit/cor/f3/util/Find.java
+++ app/src/main/java/edu/mit/cor/f3/util/Find.java
@@ -13,13 +13,15 @@ public class Find
 {
 
     public static void main(String[] args) throws Exception {
-        String input = "W20";
-        URL search = new URL("http://m.mit.edu/apis/maps/places/?q=" + URLEncoder.encode(input, "UTF-8"));
+        boolean num = true;
+        String input = "9";
+        URL search = new URL("http://m.mit.edu/apis/maps/places?q=" + URLEncoder.encode(input, "UTF-8"));
         URLConnection c = search.openConnection();
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(
                         c.getInputStream()));
         String inputLine = in.readLine();
+        System.out.println(inputLine);
         in.close();
 
         if (inputLine != null) {
@@ -28,12 +30,15 @@ public class Find
                 System.out.println("you botched it");
                 return;
             }
-            JsonObject data = arr.get(0).asObject();
-            System.out.println(data.getString("name", "mit"));
-            double lat = data.getDouble("lat_wgs84", 0);
-            double lon = data.getDouble("long_wgs84", 0);
-
-            System.out.println("Coordinates: " + lat + ", " + lon);
+            for(int a=0; a < arr.size(); a++) {
+                JsonObject data = arr.get(a).asObject();
+                if (num && !data.getString("bldgnum", "-1").equals(input))
+                    continue;
+                System.out.println(data.getString("name", "mit"));
+                double lat = data.getDouble("lat_wgs84", 0);
+                double lon = data.getDouble("long_wgs84", 0);
+                System.out.println("Coordinates: " + lat + ", " + lon);
+            }
         }
     }
 }
